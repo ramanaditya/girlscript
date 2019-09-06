09782fb1c9e5aec57db8efdf0b86eb1358585aee
ramanaditya <adityaraman96@gmail.com>
2019-09-07 00:29:05 +0530

    countdown corrected

--- js/default-assets/active.js
+++ js/default-assets/active.js
@@ -225,8 +225,8 @@
     // :: 13.0 Countdown Active Code
     // ****************************
     if ($.fn.countdown) {
-        $("#clock").countdown("sep 14,2019 08:30:00", function (event) {
-            $(this).html(event.strftime("<div>%m <span>Months</span></div> <div>%d <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
+        $("#clock").countdown("2019/09/14 08:30:00", function (event) {
+            $(this).html(event.strftime("<div>%m <span>Months</span></div> <div>%D <span>Days</span></div> <div>%H <span>Hours</span></div> <div>%M <span>Minutes</span></div> <div>%S <span>Seconds</span></div>"));
         });
     }
 
